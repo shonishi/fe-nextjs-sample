032fa8d9f51ab11507e27342cd3d402b62c60770
shonishi <the_dull_man@yahoo.co.jp>
2023-04-04 19:56:01 +0900

    reduxを導入し、re-ducksパタンを適用

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
+import { Counter } from '@/src/components/counter/Counter';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -38,7 +39,6 @@ export default function Home() {
             </a>
           </div>
         </div>
-
         <div className={styles.center}>
           <Image
             className={styles.logo}
@@ -117,6 +117,7 @@ export default function Home() {
             </p>
           </a>
         </div>
+        <Counter />
       </main>
     </>
   );
