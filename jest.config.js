babaed74e835a6284f6fc15c682bbbaee13dbeed
shonishi <the_dull_man@yahoo.co.jp>
2023-04-24 22:24:47 +0900

    jestのcoverage対象からstoriesを除外

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,8 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.stories.ts'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
